c6153cc0c921169a3f8ff12f209ef03f1e20352b
mrezaphlv <rezapahlevi927@gmail.com>
2026-04-04 23:32:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll animation saja
+function reveal() {
+  document.querySelectorAll(".reveal").forEach((el) => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", reveal);
